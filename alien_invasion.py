8c471e7888a49bd6723ad571fa0bd8971b4be1aa
Ryma Djoudad <zenyod85@gmail.com>
2026-04-12 16:21:33 -0400

    Limited amount of bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -69,8 +69,9 @@ class AlienInvasion:
             self.ship.moving_left = False
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
     def _update_screen(self):
