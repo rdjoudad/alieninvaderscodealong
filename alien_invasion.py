c9b1ae2993b593ce10de470a4224f916378c0261
Ryma Djoudad <zenyod85@gmail.com>
2026-04-18 22:02:30 -0400

    Added the Game Over! Section

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,9 @@ class AlienInvasion:
 
         self._create_fleet()
 
+        # Start Alien Invasion in an active state.
+        self.game_active = True
+
 
     def run_game(self):
         """Start the main loop of the game"""
@@ -79,19 +82,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
+        if self.stats.ships_left > 0:
         # Decrement ships_left.
-        self.stats.ships_left -= 1
+            self.stats.ships_left -= 1
 
         # Get rid of any remaining bullets and aliens.
-        self.bullets.empty()
-        self.aliens.empty()
+            self.bullets.empty()
+            self.aliens.empty()
 
         # Create a new fleet and center the ship.
-        self._create_fleet()
-        self.ship.center_ship()
+            self._create_fleet()
+            self.ship.center_ship()
 
         # Pause.
-        sleep(0.5)  
+            sleep(0.5)
+        else:
+            self.game_active = False  
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
