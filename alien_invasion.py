94bca91f2fd5b3c532331fa1008cc319ad54797c
Ryma Djoudad <zenyod85@gmail.com>
2026-04-18 21:00:40 -0400

    Checking if the fleet hit the edge

--- alien_invasion.py
+++ alien_invasion.py
@@ -47,9 +47,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
             print(len(self.bullets)) 
-                
+
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
         self.aliens.update()        
         
     
@@ -107,6 +108,19 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
