ba4a49453dc7db4769a4e32fb9dfc4be453d2d67
Ryma Djoudad <zenyod85@gmail.com>
2026-04-18 21:02:20 -0400

    Added collision stuff

--- alien_invasion.py
+++ alien_invasion.py
@@ -40,13 +40,16 @@ class AlienInvasion:
     def _update_bullets(self):
         """Update position of bullets and get rid of old bullets."""
         # Update bullet positions.
-     
-
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            print(len(self.bullets)) 
+            print(len(self.bullets))
+
+        # Check for any bullets that have hit aliens.
+        #   If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True) 
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
