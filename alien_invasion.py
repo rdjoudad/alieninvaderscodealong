bb9c4b8543292a2e0d5aa036e486c836f14dbb15
Ryma Djoudad <zenyod85@gmail.com>
2026-04-18 20:22:38 -0400

    Added alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
+
 
 
 class AlienInvasion:
@@ -21,6 +23,9 @@ class AlienInvasion:
 
         self.ship = Ship(ai_game=self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
 
     def run_game(self):
@@ -73,6 +78,11 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
     def _update_screen(self):
@@ -81,6 +91,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
+        
         pygame.display.flip()
 
 
