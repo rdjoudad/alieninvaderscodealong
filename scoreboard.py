57e1b07184d7a45718a4613d7be7400cb6d7850a
Ryma Djoudad <zenyod85@gmail.com>
2026-04-25 23:45:43 -0400

    Added the ships on the screen

--- scoreboard.py
+++ scoreboard.py
@@ -41,6 +41,7 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
 
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
