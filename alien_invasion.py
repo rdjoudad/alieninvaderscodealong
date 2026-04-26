c2754efdd2d123e08cc1f9b6336033aada65f0ac
Ryma Djoudad <zenyod85@gmail.com>
2026-04-25 22:43:56 -0400

    Printing alien points

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,6 +72,13 @@ class AlienInvasion:
         #   If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(
                 self.bullets, self.aliens, True, True) 
+        
+        if collisions:
+         for aliens in collisions.values():
+            self.stats.score += self.settings.alien_points * len(aliens)
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
+            
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
@@ -141,6 +148,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             # Reset the game statistics.
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
